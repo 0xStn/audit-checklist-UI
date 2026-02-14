813d587476bac743c6834619b1d215733dffdc6d
0xSTN <ahmedstegen1@gmail.com>
2026-02-14 21:56:18 +0200

    Fix: missing vue plugin in vite config

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,9 +4,10 @@ import path from "path";
 
 export default defineConfig({
   base: "/audit-checklist-UI/",
-    server: {
-        fs: {
-            allow: [path.resolve(__dirname, "..")]
-        }
+  plugins: [vue()],
+  server: {
+    fs: {
+      allow: [path.resolve(__dirname, "..")]
     }
+  }
 });
